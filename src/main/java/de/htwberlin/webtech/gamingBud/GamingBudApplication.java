41aeb12ec3a55ca2a4e573a2f1fb85f4def89b6f
Mohanad Al-Abiad <weiss97m@gmail.com>
2022-05-16 22:31:30 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/gamingBud/GamingBudApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.gamingBud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingBudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingBudApplication.class, args);
+	}
+
+}
